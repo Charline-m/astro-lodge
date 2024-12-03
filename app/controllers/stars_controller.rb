b47dd17f918aec6848b91cd7695b9970c6436102
Claire Cagnat <cagnatclaire@gmail.com>
2024-12-03 13:23:46 +0100

    Ajout des actions index, show, new et create pour StarsController

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,13 +1,28 @@
 class StarsController < ApplicationController
   def index
+    @stars = Star.all
   end
 
   def show
+    @star = Star.find(params[:id])
   end
 
   def new
+    @star = Star.new
   end
 
   def create
+    @star = Star.new(star_params)
+    if @star.save
+      redirect_to stars_path, notice: "Astre créé avec succès"
+    else
+      render :new, alert: "Erreur lors de la création"
+    end
+  end
+
+  private
+
+  def star_params
+    params.require(:star).permit(:name, :description)
   end
 end
