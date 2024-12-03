12f57efbc2fb6828b6a72d2563dfa113b3ed4185
Claire Cagnat <cagnatclaire@gmail.com>
2024-12-03 15:32:35 +0100

    modifiction params.require sur stars-controller

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -23,6 +23,6 @@ class StarsController < ApplicationController
   private
 
   def star_params
-    params.require(:star).permit(:name, :description)
+    params.require(:star).permit(:name, :description, :type, :price)
   end
 end
