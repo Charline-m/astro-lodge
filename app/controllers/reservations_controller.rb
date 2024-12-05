4e215ece93d2551689c4f4253f0c3197e0c5d3aa
max poirier <maxpoirier030395@gmail.com>
2024-12-05 16:58:26 +0100

    dashboard-d2

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,20 +8,34 @@ class ReservationsController < ApplicationController
 
   def create
     @star = Star.find(params[:star_id])
-    @reservation = @star.reservations.new(reservations_params)
+    @reservation = Reservation.new(reservations_params)
+    @reservation.star = @star
     @reservation.user = current_user
-
-      if @star.reservations.where("start_date < ? AND end_date > ?", @reservation.end_date, @reservation.start_date).exists?
-        flash[:alert] = "L'astre est déjà réservé pour ces dates."
-        redirect_to star_path(@star)
-      elsif @reservation.save
-        flash[:notice] = "Réservation confirmée !"
-        redirect_to dashboard_path
-      else
-        flash[:alert] = "Erreur dans la réservation."
-        render "stars/show"
-      end
+    @reservation.start_date = params[:reservation][:start_date]
+    @reservation.end_date = params[:reservation][:end_date]
+    # Vérifiez les conflits de réservation
+    if @star.reservations.where(
+      "(start_date <= ? AND end_date >= ?) OR
+       (start_date <= ? AND end_date >= ?) OR
+       (start_date >= ? AND end_date <= ?)",
+      @reservation.start_date, @reservation.start_date,
+      @reservation.end_date, @reservation.end_date,
+      @reservation.start_date, @reservation.end_date
+      ).exists?
+      Rails.logger.debug "Conflit détecté : redirection en cours."
+      # flash[:alert] = "L'astre est déjà réservé pour ces dates."
+      redirect_to stars_path, notice: "L'astre est déjà réservé pour ces dates." and return
+    end
+    # Sauvegarder la réservation
+    if @reservation.save
+      # flash[:notice] = "Réservation confirmée !"
+      redirect_to dashboard_path, notice: "Réservation confirmée !" and return
+    else
+      # flash[:alert] = "Erreur dans la réservation."
+      render "stars/show", notice: "Erreur dans la réservation."
+    end
   end
+
 
   def destroy
     @reservation = Reservation.find(params[:id])
